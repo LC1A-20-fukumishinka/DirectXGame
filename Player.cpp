2773ef2d5cd5235c3026da62bc550fc9dcd26330
LE2A_04_OGAWA <k020g1408@g.neec.ac.jp>
2022-02-01 21:07:28 +0900

    Player少しだけ内容変更

--- Player.cpp
+++ Player.cpp
@@ -58,7 +58,7 @@ Player::~Player()
 	delete StopSE;
 }
 
-void Player::Init(const Camera &camera, const XMFLOAT3 &pos)
+void Player::Init(const Camera& camera, const XMFLOAT3& pos)
 {
 	this->pos = pos;
 	//direction = { 0,0,0 };
@@ -96,7 +96,7 @@ void Player::Init(const Camera &camera, const XMFLOAT3 &pos)
 	clear.SpriteUpdate();
 }
 
-void Player::Input(const Camera &camera)
+void Player::Input(const Camera& camera)
 {
 	/*---コントローラー操作用---*/
 	if (input->isPadConnect())
@@ -234,7 +234,7 @@ void Player::Input(const Camera &camera)
 	}
 }
 
-void Player::Update(Camera &camera, const XMFLOAT3 &enemyPos)
+void Player::Update(Camera& camera, const XMFLOAT3& enemyPos)
 {
 	//移動処理
 	if (!isDead && !isClear)
@@ -290,7 +290,7 @@ void Player::Update(Camera &camera, const XMFLOAT3 &enemyPos)
 	obj.position = pos;
 	obj.Update(camera);
 
-	/*---キー操作用---*/
+	/*-----操作-----*/
 	/*時間停止*/
 
 	//時間停止外
@@ -440,7 +440,7 @@ void Player::Update(Camera &camera, const XMFLOAT3 &enemyPos)
 	}
 }
 
-void Player::Draw(const PipeClass::PipelineSet &pipelineSet)
+void Player::Draw(const PipeClass::PipelineSet& pipelineSet)
 {
 	//適当なモデルで代用する
 	//if (IsDead()) return;
@@ -465,7 +465,7 @@ void Player::Finalize()
 {
 }
 
-void Player::PushBack(const XMFLOAT3 &enemyPos)
+void Player::PushBack(const XMFLOAT3& enemyPos)
 {
 	//半径
 	float r = 13.0f + 14.0f;
@@ -492,7 +492,7 @@ void Player::PushBack(const XMFLOAT3 &enemyPos)
 	}
 }
 
-void Player::DeathEffect(Camera &camera)
+void Player::DeathEffect(Camera& camera)
 {
 	if (IsDead())
 	{
@@ -524,7 +524,7 @@ void Player::Damaged()
 	}
 }	//HPを減らす
 
-bool Player::SetGoalAndCheak(const XMFLOAT3 &lowerLeft, const XMFLOAT3 &upperRight)
+bool Player::SetGoalAndCheak(const XMFLOAT3& lowerLeft, const XMFLOAT3& upperRight)
 {
 	if (lowerLeft.x <= pos.x &&
 		pos.x <= upperRight.x &&
@@ -537,7 +537,7 @@ bool Player::SetGoalAndCheak(const XMFLOAT3 &lowerLeft, const XMFLOAT3 &upperRig
 	return false;
 }
 
-void Player::ClearEffect(Camera &camera, bool setGoalAndCheak)
+void Player::ClearEffect(Camera& camera, bool setGoalAndCheak)
 {
 	if (setGoalAndCheak)
 	{
